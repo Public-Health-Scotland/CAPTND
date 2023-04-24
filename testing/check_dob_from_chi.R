016ce19ce91e0c30d5aa5db1b1b7fcbac4346b48
joana-bsilvestre <joana.bittenourtsilvestre@phs.scot>
2023-04-24 12:03:15 +0100

    corrected return on HB names and added testing CHI DOB

---
+++ testing/check_dob_from_chi.R
@@ -0,0 +1,19 @@
+##############################.
+###   Compare DOB and CHI  ###
+##############################.
+
+#Compares DOB recorded to DOB from CHI
+source('setup/new_column_names_swift.R')
+library(dplyr)
+library(phsmethods)
+
+
+check_dob_from_chi <- function(df){
+  
+  df_dob=df %>%
+    mutate(dob_recorded_matches_chi = case_when(dob_from_chi(!!chi_o)==!!sym(chi_o) ~ TRUE,
+                                                dob_from_chi(!!chi_o)==!!sym(chi_o) ~ FALSE) )
+  
+  return(df_dob)
+}
+
