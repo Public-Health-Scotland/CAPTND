7a9289da08d4d0fc701c8abd2e2289efbce8a6c4
mgannon99 <maria.gannon@phs.scot>
2023-05-02 15:30:57 +0100

    update to check_chi

--- functions/testing/check_chi.R
+++ functions/testing/check_chi.R
@@ -2,7 +2,9 @@
 ### Check CHI #####
 ###################.
 
+#Pads all CHIs with 9 digits using chi_pad from phsmethods
 #Checks all CHIs in df using chi_check from phsmethods
+#Removes records with invalid CHIs to output file
 
 # Author: Maria Gannon
 # Date: 19/04/2023
@@ -11,6 +13,8 @@
 source('setup/new_column_names_swift.R')
 library(dplyr)
 library(phsmethods)
+library(readr)
+library(lubridate)
 
 # 2 chi_check -----------------------------------------------------
 
@@ -21,6 +25,21 @@ check_chi_captnd <- function(df){
            validity = chi_check(!!sym(chi_o))) %>%
     filter(validity == 'Valid CHI') %>% 
     select(-validity)
+  
+  df_removed = setdiff(df, df_check_chi)
+  
+  df_removed_location=paste0('../../../output/removed/',
+                             'remove_unusable_chi_',
+                             today(),
+                             '.csv')
+  
+  write_csv(df_removed, df_removed_location)
+  usable_records=nrow(df_check_chi)
+  unusable_records=nrow(df_removed)
+  
+  message(paste(unusable_records, 
+                'records were removed and saved to\n',
+                df_removed_location))
   
   return(df_check_chi)
 }
