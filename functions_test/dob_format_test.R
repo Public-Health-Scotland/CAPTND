ab9504f12781f63bff312107972dff3785920f9f
mgannon99 <maria.gannon@phs.scot>
2023-05-11 14:44:53 +0100

    test for dob format for test dataset

---
+++ functions_test/dob_format_test.R
@@ -0,0 +1,23 @@
+#######################################################.
+### Test for correcting dob format in test dataset  ###
+#######################################################.
+
+
+# 1 Load function ----------------------------------------------------------
+
+source('functions/testing/dob_format_testdata.R')
+library(readr)
+library(dplyr)
+library(lubridate)
+
+
+
+# 2 Test --------------------------------------------------------------
+
+df=read_csv('../../../data/testDataset.csv')
+
+df_test=dob_format(df)
+
+#expected: 6 NAs, 72 formatted yyyy-mm-dd
+
+
