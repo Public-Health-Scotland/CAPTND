a38016bd3667f248688d5ced8444f31ff4852c05
joana-bsilvestre <joana.bittenourtsilvestre@phs.scot>
2023-05-12 15:50:47 +0100

    Added complete dob from chi in case dob is NA

--- functions/testing/check_dob_from_chi.R
+++ functions/testing/check_dob_from_chi.R
@@ -20,7 +20,7 @@ check_dob_from_chi <- function(df){
   
   df_dob = df %>%
     mutate(!!chi_o := as.character(!!sym(chi_o)),
-           !!dob_from_chi_o := format(dob_from_chi(!!sym(chi_o)),'%d/%m/%Y'),
+           !!dob_from_chi_o := dob_from_chi(!!sym(chi_o)),
            !!dob_recorded_matches_chi_o := case_when(!!sym(dob_from_chi_o) == !!sym(dob_o) ~ TRUE,
                                                      !!sym(dob_from_chi_o) != !!sym(dob_o) ~ FALSE),
            .after = !!dob_o)
