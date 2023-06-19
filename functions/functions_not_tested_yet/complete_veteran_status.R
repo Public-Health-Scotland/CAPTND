a50faac4870004130120d809198d30d4f439ead1
joana-bsilvestre <joana.bittenourtsilvestre@phs.scot>
2023-06-19 17:11:48 +0100

    vet status completion function

--- functions/functions_not_tested_yet/complete_veteran_status.R
+++ functions/functions_not_tested_yet/complete_veteran_status.R
@@ -15,36 +15,71 @@ library(lubridate)
 # 2 Function --------------------------------------------------------------
 
 complete_veteran_status <- function(df){
-  
+  #test
+  # df=read_csv('../../../data/testDataset_vet.csv')
+  # vet_edited_o='vet_edited'
+  # vet_edited_counts_o='vet_edited_counts'
+  # first_vet_o='first_vet'
+  # last_vet_o='last_vet'
+  # 
+  # df$vet=as.numeric(df$vet)
+
   df_completed <- df %>%
-    mutate(!!vet_edited_o := case_when(!!sym(vet_o) %in% c(98,99)~ NA_integer_,
-                                        TRUE ~ !!sym(vet_o)),
-           .after=!!vet_o) %>% 
     group_by(!!sym(chi_o)) %>% 
-    mutate(!!vet_edited_counts_o := (n_distinct(!!sym(vet_edited_o))),
-           !!first_vet_o := first(!!sym(vet_edited_o), order_by = !!sym(header_date_o)),
-           !!last_vet_o := last(!!sym(vet_edited_o), order_by = !!sym(header_date_o)),
-           !!vet_edited_o := case_when(!!sym(vet_edited_counts_o)==2 &
-                                         is.na(!!sym(first_vet_o)) &
-                                         !!sym(last_vet_o)==1 ~ 1,
-                                       !!sym(vet_edited_counts_o)==2 &
-                                         !!sym(first_vet_o)==2 &
-                                         is.na(!!sym(last_vet_o)) ~ 2,
-                                       TRUE ~ !!sym(vet_edited_o)
-                                       ),
-           .after=!!vet_edited_o)
-           #doesn't work for cases where the data structure is NA 1 NA
+    mutate(!!vet_edited_o := case_when(
+      (is.na(!!sym(vet_o)) | !!sym(vet_o) %in% c(98, 99)) & 
+        lead(!!sym(vet_o), order_by=!!sym(header_date_o)) == 1 ~ 1,
+      (is.na(!!sym(vet_o)) | !!sym(vet_o) %in% c(98, 99)) & 
+        lag(!!sym(vet_o),order_by=!!sym(header_date_o)) == 2 ~ 2,
+                                       TRUE ~ !!sym(vet_o)),
+      .after=!!vet_o)
+     
 
   return(df_completed)
 }
 
 
-#just a test
-df<-  read_csv('../../../data/testDataset_lowercase.csv') %>% 
-  mutate(chi = as.character(chi)) %>% 
-  filter(issue=='one_CHI_3_vets_status')
 
-vet_edited_o='vet_edited'
-first_vet_o='first_vet'
-last_vet_o='last_vet'
-vet_edited_counts_o='vet_edited_counts'
+
+# mutate(!!vet_edited_o := case_when(!!sym(vet_o) %in% c(98,99)~ NA_integer_,
+#                                     TRUE ~ !!sym(vet_o)),
+#        .after=!!vet_o) %>% 
+
+# !!vet_edited_counts_o := (n_distinct(!!sym(vet_edited_o))),
+#        !!first_vet_o := first(!!sym(vet_edited_o), order_by = !!sym(header_date_o)),
+#        !!last_vet_o := last(!!sym(vet_edited_o), order_by = !!sym(header_date_o)),
+# firstwith=na.locf(!!sym(vet_edited_o), fromLast = TRUE, na.rm = FALSE)
+# 
+# 
+# 
+# 
+# if next non na value is 2 and previous non na value is 2  ~ 2
+# if next non na value is 2 and there is no previous non na value ~ na
+# if next non na value is 2 and previous non na value is 1 ~ na
+# if next non value is 1 ~ 1
+# 
+# 
+# !!first_vet_yes := first(!!sym(vet_edited_o)[!!sym(vet_edited_o)==1], order_by = !!sym(header_date_o)),
+
+
+
+
+
+
+
+
+
+
+# !!sym(vet_edited_counts_o)==2 &
+#                             is.na(!!sym(first_vet_o)) &
+#                             !!sym(last_vet_o)==1 ~ 1,
+#                           !!sym(vet_edited_counts_o)==2 &
+#                             !!sym(first_vet_o)==2 &
+#                             is.na(!!sym(last_vet_o)) ~ 2,
+#                           !!sym(vet_edited_counts_o)==2 &
+#                             !!sym(last_vet_o)==!!sym(first_vet_o) ~ !!sym(first_vet_o),
+# 
+# 
+#                           !!sym(vet_edited_counts_o)==3&
+#                             is.na(!!sym(first_vet_o)) &
+
