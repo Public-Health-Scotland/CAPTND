6876ea2f39b74305483d5fdbb7553aac54fdf24e
joana-bsilvestre <joana.bittenourtsilvestre@phs.scot>
2023-07-12 17:38:38 +0100

    fine tuned plots for reports

--- functions/correct_HB_names.R
+++ functions/correct_HB_names.R
@@ -36,33 +36,48 @@ correct_hb_names <- function(df){
                                   str_detect(!!sym(hb_name_o), regex('west|wi', ignore_case = TRUE)) ~ 'NHS Western Isles',
                                   str_detect(!!sym(hb_name_o), regex('2', ignore_case = TRUE)) ~ 'NHS24',
                                   str_detect(!!sym(hb_name_o), regex('scot', ignore_case = TRUE)) ~ 'NHS Scotland',
-                                  TRUE ~ !!sym(hb_name_o)))
-  
-  #Establish what names could not be corrected
-  df_wrong_hb_names <- df_hb_evaluated %>% 
-    filter(!(!!sym(hb_name_o) %in% c('NHS Ayrshire and Arran',
-                              'NHS Borders',
-                              'NHS Dumfries and Galloway',
-                              'NHS Fife',
-                              'NHS Forth Valley',
-                              'NHS Grampian',
-                              'NHS Greater Glasgow and Clyde',
-                              'NHS Highland',
-                              'NHS Lanarkshire',
-                              'NHS Lothian',
-                              'NHS Orkney',
-                              'NHS Shetland',
-                              'NHS Tayside',
-                              'NHS Western Isles',
-                              'NHS24'))) 
+                                  TRUE ~ !!sym(hb_name_o))) %>% 
+    mutate(hb_correct=case_when(!(!!sym(hb_name_o) %in% c('NHS Ayrshire and Arran',
+                                            'NHS Borders',
+                                            'NHS Dumfries and Galloway',
+                                            'NHS Fife',
+                                            'NHS Forth Valley',
+                                            'NHS Grampian',
+                                            'NHS Greater Glasgow and Clyde',
+                                            'NHS Highland',
+                                            'NHS Lanarkshire',
+                                            'NHS Lothian',
+                                            'NHS Orkney',
+                                            'NHS Shetland',
+                                            'NHS Tayside',
+                                            'NHS Western Isles',
+                                            'NHS24')) ~ FALSE,
+                                TRUE ~ TRUE),
+           .after=!!hb_name_o)
+     
 
   #Prepare df with only correct names
-  df_hb_correct <- setdiff(df_hb_evaluated,df_wrong_hb_names)
+  df_hb_correct <- df_hb_evaluated %>% 
+    filter(hb_correct) %>% 
+    select(-hb_correct)
   
   #Get wrong board names and postcodes
-  df_wrong_hb_names <- df_wrong_hb_names %>% 
-    select(!!hb_name_o,!!postcode_o) %>% 
-    distinct()
+  df_wrong_hb_names <- df_hb_evaluated %>% 
+    mutate(!!submission_date_o := ym(format(!!sym(header_date_o), "%Y-%m"))) %>% 
+    select(!!hb_name_o,!!dataset_type_o,!!submission_date_o,hb_correct) %>%
+    group_by(!!sym(dataset_type_o),!!sym(submission_date_o)) %>% 
+    mutate(!!total_rows_o := n()) %>%
+    filter(!hb_correct) %>% 
+    group_by(!!sym(dataset_type_o),!!sym(submission_date_o),!!sym(hb_name_o), hb_correct) %>% 
+    mutate(unident_hb_variation=n()) %>% 
+    group_by(!!sym(dataset_type_o),!!sym(submission_date_o),hb_correct) %>% 
+    mutate(unident_hb_total_number = sum(unident_hb_variation),
+           unident_hb_total_perc= round(unident_hb_total_number/!!sym(total_rows_o), 4)) %>% 
+    distinct() %>% 
+    inner_join(df_hb_evaluated %>% select(!!hb_name_o,!!postcode_o), by=hb_name_o) %>% 
+    ungroup()
+
+    
   
   #Get number of alternative non identifiable spellings
   weird_hb_names <- df_wrong_hb_names %>% 
@@ -73,18 +88,19 @@ correct_hb_names <- function(df){
     message(paste('All health board names have been corrected.'))
   }else{
     df_wrong_hb_names_location=paste0('../../../output/removed/',
-                                      'uninteligible_HB_names_',
+                                      'unidentifiable_HB_names_',
                                       as.character(today()),
                                       '.csv')
     
     write_csv(df_wrong_hb_names, df_wrong_hb_names_location)
     
+  
     
     message(paste0(length(weird_hb_names), 
-                  ' unidentifiable board name(s) was found:\n',
+                  ' unidentifiable board names were found:\n',
                   paste(weird_hb_names, collapse=', ' ),
-                  '\nA table with unidentifiable names and postcodes was saved at\n ',
-                  df_wrong_hb_names_location))
+                  '\nA table with unidentifiable names, postcodes and stats was saved at\n ',
+                  df_wrong_hb_names_location,'\n'))
   }
   
   
