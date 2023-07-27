e9671a505a98160762747a2038022ad66d9e24ee
joana-bsilvestre <joana.bittenourtsilvestre@phs.scot>
2023-07-27 10:00:52 +0100

    modifications of too old for camhs script

--- investigations/patients_too_old_camhs.R
+++ investigations/patients_too_old_camhs.R
@@ -1,14 +1,33 @@
-camhs_weird_dob=df_swift_clean2 %>% filter(dob<today(),dataset_type=='CAMHS')
+camhs_weird_dob=df_swift_clean %>% filter(dob<today(),dataset_type=='CAMHS')
 
-camhs_weird_dob_boards = camhs_weird_dob %>% 
-  filter(dob<ymd(20000101)) %>% 
-  select(hb_name, dataset_type, chi) %>% 
+camhs_weird_dob_boards = df_swift_clean %>% 
+  filter(dob<ymd(20000101),dataset_type=='CAMHS') %>% 
+  select(hb_name, dataset_type, chi, dob) %>% 
   distinct()
+
+
+camhs_weird_dob_boards_list <- camhs_weird_dob_boards %>% 
+  group_by(hb_name) %>% 
+  group_split()
+
+for(x in camhs_weird_dob_boards_list){
+  nameOfBoard=unique(x$hb_name)
+  write_csv(x, paste0("../../../output/investigations/tooOldForCAMHS/tooOldForCAMHS_",nameOfBoard ,".csv"))
+}
+
+
+tay=df_swift_clean %>% filter(chi %in% c('0111970547',
+                                     '1110970226',
+                                     '0206271220',
+                                     '1307870147') & dataset_type=='CAMHS')
+
+
 df_camhs_tooOld=as.data.frame(table(camhs_weird_dob_boards$hb_name))
 
 
+
 df_camhs_tooOld %>% 
-  arrange(Freq) %>% 
+  #arrange(Freq) %>% 
   ggplot( aes(x=Var1, y=Freq)) +
   geom_bar(stat="identity")+
   theme_minimal()+
