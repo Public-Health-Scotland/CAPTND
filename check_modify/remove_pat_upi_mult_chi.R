afd2d141bf7027f7c96128f9862944e32b364786
joana-bsilvestre <joana.bittenourtsilvestre@phs.scot>
2023-08-14 15:45:37 +0100

    modified reporting upi issue

--- check_modify/remove_pat_upi_mult_chi.R
+++ check_modify/remove_pat_upi_mult_chi.R
@@ -4,7 +4,7 @@
 
 library(dplyr)
 
-filter_non_unique_upi <- function(df) {
+filter_non_unique_upi <- function(df,stage_name) {
   chis_per_upi <- df %>% 
     select(!!hb_name_o, !!dataset_type_o, !!upi_o, !!chi_o) %>% 
     distinct() %>% 
@@ -26,18 +26,8 @@ filter_non_unique_upi <- function(df) {
   df_filtered <- df %>% 
     anti_join(chis_per_upi_with_chis_to_remove, by=c(hb_name_o, dataset_type_o, upi_o, chi_o))
   
-  
-  chis_per_upi_with_chis_to_remove_stats <- chis_per_upi_with_chis_to_remove %>% 
-    inner_join(df, by=c(hb_name_o, dataset_type_o, upi_o, chi_o)) %>% 
-    group_by(!!sym(hb_name_o),!!sym(dataset_type_o)) %>% 
-    summarise(removed_records=n()) %>% 
-    ungroup()
-  
-  write_csv(chis_per_upi_with_chis_to_remove_stats, 
-            paste0("../../../output/removed/multi_chis_per_upi_",today(),".csv"))
-  
-  message(paste("Stats on removed files due to UPI not being unique to patient, and CHI being NA are saved in\n",
-                paste0("../../../output/removed/multi_chis_per_upi_",today(),".csv")))
+  report_upi_mult_chi(df,chis_per_upi_with_chis_to_remove, stage_name)
+ 
   
   return(df_filtered)
 }
---
+++ reporting/report_removed_upi_mult_chi_NT.R
@@ -0,0 +1,98 @@
+#######################################################################.
+###  Percentage of removed rows due to UPI not being unique report  ###
+#######################################################################.
+
+# 1 Load libraries and packages ---------------------------------------------
+library(dplyr)
+library(lubridate)
+library(readr)
+source('config/new_colnames.R')
+
+
+# 2 Function ----------------------------------------------------------------
+
+report_upi_mult_chi <- function(df, chis_per_upi_with_chis_to_remove, saveName) {
+  
+  timePeriod=1 #time in years that the report will plotted. 
+  level_order <- c('NHS Ayrshire and Arran',
+                   'NHS Borders',
+                   'NHS Dumfries and Galloway',
+                   'NHS Fife',
+                   'NHS Forth Valley',
+                   'NHS Grampian',
+                   'NHS Greater Glasgow and Clyde',
+                   'NHS Highland',
+                   'NHS Lanarkshire',
+                   'NHS Lothian',
+                   'NHS Orkney',
+                   'NHS Shetland',
+                   'NHS Tayside',
+                   'NHS Western Isles',
+                   'NHS24',
+                   'NHS Scotland')
+  
+  df_total_rows = df %>% 
+    mutate(!!submission_date_o := ym(format(!!sym(header_date_o), "%Y-%m")),
+           .after=!!file_id_o) %>% 
+    group_by(!!sym(hb_name_o),!!sym(dataset_type_o),!!sym(submission_date_o)) %>% 
+    summarise(!!total_rows_o:=n(),
+              .groups = "drop")
+  
+  df_stats <- chis_per_upi_with_chis_to_remove %>%
+    inner_join(df, by=c(hb_name_o, dataset_type_o, upi_o, chi_o)) %>% 
+    mutate(!!submission_date_o := ym(format(!!sym(header_date_o), "%Y-%m")),
+           .after=!!file_id_o) %>% 
+    group_by(!!sym(hb_name_o),!!sym(dataset_type_o), !!sym(submission_date_o)) %>% 
+    summarise(removed_rows=n()) %>% 
+    inner_join(df_total_rows, by=c(hb_name_o,dataset_type_o,submission_date_o)) %>% 
+    ungroup() %>% 
+    mutate(perc_removed=(removed_rows*100)/!!sym(total_rows_o))
+  
+  #plot removed records
+  df_stats %>% filter(!!sym(submission_date_o)>(max(!!sym(submission_date_o))- years(timePeriod))) %>% 
+    mutate(!!submission_date_o:=ym(format(!!sym(submission_date_o), "%Y-%m"))) %>% 
+    ggplot( aes(x=submission_date, y=perc_removed, group=dataset_type, colour=dataset_type)) +
+    geom_line()+
+    geom_point()+
+    theme_minimal()+
+    ylab("Removed records w")+
+    xlab("Submission date")+
+    theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))+
+    scale_x_date(
+      minor_breaks = NULL,
+      breaks = seq.Date(
+        from = min(df_stats$submission_date),
+        to = max(df_stats$submission_date),
+        by = "month"))+
+    labs(colour= "Dataset type")+
+    facet_wrap(~factor(hb_name, levels=c(level_order)))+
+    theme(plot.margin = unit(c(1,0.5,0.5,0.5), "cm"))+
+    theme(legend.position="bottom")
+  
+  savingLocation <- paste0("../../../output/removed/", 
+                           saveName,
+                           "_removed_upi_multiple_chi_")
+  
+  ggsave(paste0(savingLocation,
+                'plot_',
+                as.character(today()),
+                ".png"),
+         width = 27,
+         height = 20,
+         units = c("cm"),
+         dpi = 300,
+         bg='white')
+  
+  
+  write_csv(df_stats, paste0(savingLocation,
+                             "table_",
+                             as.character(today()),
+                             ".csv"))
+  
+  message(paste0('Stats on removed rows with UPIs associated with multiple patients and no CHI was saved to\n',
+                 savingLocation, 
+                 "{table/plot}",
+                 as.character(today()),
+                 "{.csv/.png}\n"))
+  
+}
