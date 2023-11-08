394015935634bfaa831cbdcc295ffc0fa20aee53
joana-bsilvestre <joana.bittenourtsilvestre@phs.scot>
2023-11-08 10:21:52 +0000

    added col had_first_treat_appt

--- config/new_colnames.R
+++ config/new_colnames.R
@@ -112,7 +112,7 @@ dob_verified_o <- "dob_verified" #dob that has been confirmed by chi
 sex_reported_o <- "sex_reported" # sex reported by patient/GP/consultant/etc
 ref_rec_date_opti_o <- "ref_rec_date_opti" # optimised ref rec date - if rec date is NA, use ref date instead if available
 rtt_eval_o <- "rtt_eval" #if it is possible to do RTT for record
-
+had_first_treat_appt_o <- "had_first_treat_appt" # TRUE/FALSE if a pathway had a treatment appt that was attended
 
 
 
