3cf36f62dc861a5b44915229de9119d10cfbcca8
joana-bsilvestre <joana.bittenourtsilvestre@phs.scot>
2023-11-16 12:38:22 +0000

    we made product one, still missing minor adjustments in caption, title and export

--- data_quality/product1.R
+++ data_quality/product1.R
@@ -11,42 +11,58 @@
 #hb order
 #df
 
+library(dplyr)
+library(lubridate)
+library(arrow)
+library(ggplot2)
+
+#read csv of removed rows
+df_camhs <- read_csv_arrow(paste0(stats_removed_dir,
+                            '/CAMHS_removed_rows_breakdowntable_month.csv')) 
+df_pt <- read_csv_arrow(paste0(stats_removed_dir,
+                                  '/PT_removed_rows_breakdowntable_month.csv')) 
 
 
 
-
-
-ggplot_heatmap_known <- function(df, chart_value){
+df <- bind_rows(df_camhs,df_pt) %>% 
+  group_by(!!!syms(c(dataset_type_o,hb_name_o,submission_date_o))) %>% 
+  summarise(total_rows_sum=sum(!!sym(total_rows_o)),
+            remaining_rows_sum=total_rows_sum-(sum(removed_rows)),
+         .groups = 'keep') %>% 
+  mutate(remaining_rows_perc=round(remaining_rows_sum/total_rows_sum*100, 1)) %>% 
+  ungroup() %>% 
+  mutate(traffic_light=case_when(remaining_rows_perc > 89.9 ~ '90 to 100%',
+                                 remaining_rows_perc <= 89.9 & 
+                                   remaining_rows_perc >= 70 ~ '70 to 89.9%',
+                                 remaining_rows_perc <70 ~ '0 to 69.9%')) %>% 
+  filter(!!sym(submission_date_o)>max(!!sym(submission_date_o))-months(12))
   
+
+traffic_light_colours <- c("90 to 100%" = "#9CC951", # green 80%
+                           "70 to 89.9%"="#B3D7F2",#blue
+                           "0 to 69.9%"="#D26146") #rust 80%
+
+
+
   chart_known <- df %>% 
-    #filter(Value == chart_value) %>% 
-    mutate(#Variable = fct_rev(Variable),
-           HB = factor(HB, levels = vec_hb_order_avg)) %>%  #hb order
-    ggplot(aes(x = HB, y = Variable, fill = prop_group)) + 
-    geom_tile(width = 1, height = 1, size = .25, color = "black")+ 
-    geom_text(aes(label = Proportion), size = 2)+
-    scale_fill_manual(values = colors, name = 'Known\nRecords (%)', drop = FALSE)+
-    scale_x_discrete(position = "top")+
-    scale_y_discrete(labels=c("RECEIVED_DATE" = expression(bold(RECEIVED_DATE)), 
-                              "APP_DATE" = expression(bold(APP_DATE)),
-                              "UNA_START_DATE" = expression(bold(UNA_START_DATE)),
-                              "DIAG1" = expression(bold(DIAG1)), 
-                              "MEASURES1" = expression(bold(MEASURES1)),
-                              "DISCHARGE_DATE" = expression(bold(DISCHARGE_DATE)),
-                              parse = TRUE))+
-    theme(axis.text.x = element_text(angle = 45, vjust = 0.5, hjust=0),
+    mutate(!!sym(hb_name_o) := factor(!!sym(hb_name_o), levels = rev(level_order))) %>%  
+    ggplot(aes_string(y = hb_name_o, x = submission_date_o, fill = 'traffic_light')) + 
+    geom_tile(width = 20, height = 1, size = .25, color = "black")+ 
+    geom_text(aes(label = remaining_rows_perc), size = 2)+
+    scale_fill_manual(values = traffic_light_colours, name = 'remaining rows (%)', drop = FALSE)+
+    scale_x_date(#position = "top",
+                 date_breaks = "1 month",
+                 date_labels = "%b\n%y")+
+    theme(#axis.text.x = element_text(angle = 0, vjust = 0, hjust=0),
           legend.key = element_rect(fill = "white", colour = "black"),
           plot.caption = element_text(hjust = 0))+
-    facet_wrap(~ DATASET)+
-    labs(title = paste0("CAPTND: Known Records by HB and DATASET (", month_word_end, " submissions)"),
-         subtitle = "Known records = valid and meaningful values",
+    facet_wrap(~ dataset_type)+
+    labs(title = paste0("CAPTND: remaining rows by HB and DATASET by month"),
+         subtitle = "something",
          x = NULL,
-         y = "Variables",
-         caption = "NB The variable names in bold have had all NA values removed during data preperation - their values will always be 100%")+
-    geom_vline(xintercept = c(1.5, 3.5, 5.5), colour = "white", size = 1)
+         y = NULL)
   
-  return(chart_known)
-  
-}
+
+  chart_known
 
 
