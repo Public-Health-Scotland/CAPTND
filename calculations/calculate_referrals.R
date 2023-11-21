fbb637a3cb2f850e9c630cea79129a58d8f0f6b4
joana-bsilvestre <joana.bittenourtsilvestre@phs.scot>
2023-11-21 10:07:09 +0000

    added age group to referrals and appointments calculations

--- calculations/calculate_referrals.R
+++ calculations/calculate_referrals.R
@@ -40,23 +40,23 @@ calculate_referrals <- function(df, extractDate) {
   df_referrals_details=df %>%
     filter(!is.na(!!sym(ref_acc_o))) %>% 
     mutate(!!referral_month_o := floor_date(!!sym(ref_rec_date_opti_o), 'month')) %>% 
-    select(all_of(c(data_keys,ref_acc_o, referral_month_o,simd_quintile_o, sex_reported_o))) %>% 
+    select(all_of(c(data_keys,ref_acc_o, referral_month_o,simd_quintile_o, sex_reported_o, age_group_o))) %>% 
     distinct() %>% 
     group_by(!!sym(referral_month_o),!!sym(hb_name_o),!!sym(dataset_type_o),
-             !!sym(ref_acc_o), !!sym(simd_quintile_o), !!sym(sex_reported_o)) %>% 
+             !!sym(ref_acc_o), !!sym(simd_quintile_o), !!sym(sex_reported_o), !!sym(age_group_o)) %>% 
     summarise(n=n(), .groups = 'drop') %>% 
     mutate(!!ref_acc_o:=case_when(!!sym(ref_acc_o)==1 ~ 'accepted',
                                   !!sym(ref_acc_o)==2 ~ 'not accepted',
                                   !!sym(ref_acc_o)==3 ~ 'pending')) %>% 
     group_by(!!sym(referral_month_o),!!sym(hb_name_o),!!sym(dataset_type_o),
-             !!sym(simd_quintile_o), !!sym(sex_reported_o)) %>%
+             !!sym(simd_quintile_o), !!sym(sex_reported_o), !!sym(age_group_o)) %>%
     bind_rows(summarise(.,
                         across(where(is.numeric), sum),
                         across(!!ref_acc_o, ~"total"),
                         .groups = "drop"))
   
   write_csv_arrow(df_referrals, paste0(referrals_dir,'/referrals.csv'))
-  write_csv_arrow(df_referrals_details, paste0(referrals_dir,'/referrals_details.csv'))
+  write_csv_arrow(df_referrals_details, paste0(referrals_dir,'/referrals_sex_age_simd.csv'))
   
   message(paste('Your files are in',referrals_dir))
   
