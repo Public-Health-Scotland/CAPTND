bb47c8188b4d50b312a9c5faaa750408ab1eb0d9
charlie-smith <charlie.smith2@phs.scot>
2024-02-08 13:25:03 +0000

    Rprofile - load packages before functions. control_swift - update setup folder numbering.

--- 01_control/control_swift.R
+++ 01_control/control_swift.R
@@ -82,7 +82,7 @@ read_clean_captnd_data <- function() {
   start_time <- Sys.time()
   
   # pull swift data from database (run every time updated data required)
-  source("./setup/swift_pull_save_parquet.R")
+  source("./02_setup/swift_pull_save_parquet.R")
   
   # load saved parquet files
  
