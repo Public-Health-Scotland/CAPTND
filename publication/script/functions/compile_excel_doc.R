1fcab97433528017b15c68fc2b5aa1fcf276d051
charlie-smith <charlie.smith2@phs.scot>
2024-02-08 08:51:07 +0000

    Update to CAPTND publication scripts - compile supplementary data tables into seperate CAMHS and PT excel files.

--- publication/script/functions/compile_excel_doc.R
+++ publication/script/functions/compile_excel_doc.R
@@ -9,14 +9,52 @@
 
 compile_excel_doc <- function(){
   
-  # load data
+  # load CAMHS data
   table_refs_camhs <- read_parquet(paste0(data_working_safe, 'table_refs_quarterly_CAMHS.parquet'))
+  
+  refs_monthly_camhs <- read_parquet(paste0(data_working_safe, 'refs_monthly_sco.parquet')) |> 
+    filter(!!sym(dataset_type_o) == "CAMHS")
+  
+  refs_sex_age_camhs <- read_parquet(paste0(data_working_safe, 'refs_sex_age.parquet')) |> 
+    filter(!!sym(dataset_type_o) == "CAMHS")
+  
+  ref_simd_camhs <- read_parquet(paste0(data_working_safe, 'refs_simd.parquet')) |> 
+    filter(!!sym(dataset_type_o) == "CAMHS" &
+          !!sym(simd_quintile_o) %in% c(1, 5)) |> 
+    arrange(!!sym(simd_quintile_o)) |> 
+    mutate(!!referral_month_o := as.character(!!sym(referral_month_o)))
+  
+  
+  # save CAMHS doc
+  tabs <- list(refs_camhs = table_refs_camhs, 
+               refs_monthly = refs_monthly_camhs, 
+               refs_sex_age = refs_sex_age_camhs, 
+               ref_simd = ref_simd_camhs)
+  
+  rio::export(tabs, paste0(data_working_safe, 'data_for_pub_CAMHS.xlsx'))
+  
+  
+  # load PT data
   table_refs_pt <- read_parquet(paste0(data_working_safe, 'table_refs_quarterly_PT.parquet'))
   
-  # save in doc
-  tabs <- list(refs_camhs = table_refs_camhs, 
-               refs_pt = table_refs_pt)
+  refs_monthly_pt <- read_parquet(paste0(data_working_safe, 'refs_monthly_sco.parquet')) |> 
+    filter(!!sym(dataset_type_o) == "PT")
   
-  rio::export(tabs, paste0(data_working_safe, 'data_for_publication.xlsx'))
+  refs_sex_age_pt <- read_parquet(paste0(data_working_safe, 'refs_sex_age.parquet')) |> 
+    filter(!!sym(dataset_type_o) == "PT")
+  
+  ref_simd_pt <- read_parquet(paste0(data_working_safe, 'refs_simd.parquet')) |> 
+    filter(!!sym(dataset_type_o) == "PT" &
+             !!sym(simd_quintile_o) %in% c(1, 5)) |> 
+    arrange(!!sym(simd_quintile_o)) |> 
+    mutate(!!referral_month_o := as.character(!!sym(referral_month_o)))
+  
+  # save CAMHS doc
+  tabs <- list(refs_pt = table_refs_pt, 
+               refs_monthly = refs_monthly_pt, 
+               refs_sex_age = refs_sex_age_pt, 
+               ref_simd = ref_simd_pt)
+  
+  rio::export(tabs, paste0(data_working_safe, 'data_for_pub_PT.xlsx'))
   
 }
