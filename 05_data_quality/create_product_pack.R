04834f60222a2bc075d2fb9e284f5be7f560c533
Bex Madden <bex.madden@phs.scot>
2024-03-07 15:11:03 +0000

    Bex coding review updates

--- 05_data_quality/create_product_pack.R
+++ 05_data_quality/create_product_pack.R
@@ -1,4 +1,15 @@
+
+###########################.
+### Create Product Pack ###
+###########################.
+
+# Author: Bex Madden
+# Date: 2024-03-07
+
 library(openxlsx)
+
+
+# 1. Derive dates ---------------------------------------------------------
 
 
 #get most recent and earliest dates for which products have been created 
@@ -13,6 +24,10 @@ library(openxlsx)
   earliest_date <- files[1] # creates object with earliest product date only
 
 
+
+# 2. Populate workbook ---------------------------------------------------------
+
+
 # pull template excel workbook containing products 1, 2, 3 - narrative and readme in template
 
 wb <- loadWorkbook(paste0("../../../output/product_pack_working/template_products.xlsx"))
@@ -22,31 +37,25 @@ wb <- loadWorkbook(paste0("../../../output/product_pack_working/template_product
 insertImage(wb, "Data Retention", paste0(product1_dir, "/product1.png"),
             startRow = 18, startCol = 2, width = 29, height = 13.5, units = "cm")
 
-insertImage(wb, "RTT summary", paste0(product2_dir, "/product2_heatmap_", latest_date, ".png"), #insert date conditional
+insertImage(wb, "RTT summary", paste0(product2_dir, "/product2_heatmap_", latest_date, ".png"), 
             startRow = 21, startCol = 2, width = 24, height = 13.5, units = "cm")
-insertImage(wb, "RTT summary", paste0(product2_dir, "/product2_heatmap_", earliest_date, ".png"), #insert date conditional
+insertImage(wb, "RTT summary", paste0(product2_dir, "/product2_heatmap_", earliest_date, ".png"), 
             startRow = 37, startCol = 2, width = 24, height = 13.5, units = "cm")
 
-insertImage(wb, "Data Completion", paste0(product3_dir, "/product3_closed_cases_until_", latest_date, ".png"), #insert date conditional
+insertImage(wb, "Data Completion", paste0(product3_dir, "/product3_closed_cases_until_", latest_date, ".png"), 
             startRow = 16, startCol = 2, width = 30, height = 15, units = "cm")
 
-# # If wanting to add a dated comment above main narrative
-# prod1_narrative <- paste0("Product 1 test ", latest_date)
-# prod2_narrative <- paste0("Product 2 test ", latest_date)
-# prod3_narrative <- paste0("Product 3 test ", latest_date)
-# 
-# # insert narrative text to each sheet
-# writeData(wb, "Data Retention", x = prod1_narrative, startCol = 2, startRow = 6) #, headerStyle = my_fontsize
-# writeData(wb, "RTT summary", x = prod2_narrative, startCol = 2, startRow = 6) #, headerStyle = my_fontsize
-# writeData(wb, "Data Completion", x = prod3_narrative, startCol = 2, startRow = 6) #, headerStyle = my_fontsize
+# If wanting to add a dated comment above main narrative
+prod1_narrative <- paste0("The data shown relates to CAPTND data retention in the 12 months prior to ", latest_date, ".")
+prod2_narrative <- paste0("The data shown relates to waiting time calculations in CAPTND, from August 2021 up until ", latest_date, " and ", earliest_date, " respectively.")
+prod3_narrative <- paste0("The data shown relates to completed patient pathways in CAPTND up until ", latest_date)
+ 
+# insert narrative text to each sheet
+writeData(wb, "Data Retention", x = prod1_narrative, startCol = 2, startRow = 6) #, headerStyle = my_fontsize
+writeData(wb, "RTT summary", x = prod2_narrative, startCol = 2, startRow = 6) #, headerStyle = my_fontsize
+writeData(wb, "Data Completion", x = prod3_narrative, startCol = 2, startRow = 6) #, headerStyle = my_fontsize
 
 
 saveWorkbook(wb, paste0(external_reports_dir, "/Product_Pack_", latest_date, ".xlsx"), overwrite = TRUE)
-
-
-
-
-
-
 
 
