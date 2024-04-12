01e23cec68c80b7a75b1c8fc38680f078ff35632
charlie-smith <charlie.smith2@phs.scot>
2024-04-12 14:55:22 +0100

    Finalised dq data prep

--- 10_pre_shorewise_scripts/assess_rej_reason.R
+++ 10_pre_shorewise_scripts/assess_rej_reason.R
@@ -1,7 +1,7 @@
 
-#############################.
+###############################.
 ### Assess Rejection Reason ###
-#############################.
+###############################.
 
 # Author: Charlie Smith
 # Date: 2024-04-11
@@ -14,10 +14,15 @@ assess_rej_reason <- function(df){
     mutate(REJ_REASON = str_pad(REJ_REASON, 2, pad = "0")) %>% 
     pull()
   
-  df_rej_reason <- df
+  df_rej_reason <- df |> 
     mutate(check_rej_reason = case_when(
-      
-      ))
+      !!sym(ref_acc_o) == '01' & is.na(!!sym(ref_rej_reason_o)) ~ "missing but valid", # accepted and no rejection reason = valid
+      !!sym(ref_acc_o) == '02' & is.na(!!sym(ref_rej_reason_o)) ~ "missing", # rejected & rej reason is missing = missing
+      !!sym(ref_acc_o) == '02' & !!sym(ref_rej_reason_o) %in% vec_rej_reason ~ "valid",# rej reason is included in list of valid reasons = valid
+      !!sym(ref_acc_o) == '03' & is.na(!!sym(ref_rej_reason_o)) ~ "missing but valid", # pending and rej reason is missing = valid
+      is.na(!!sym(ref_acc_o)) & is.na(!!sym(ref_rej_reason_o)) ~ "missing",
+      !!sym(ref_rej_reason_o) %in% c("06", "07") ~ "not known",
+      TRUE ~ "Invalid"))
     
   return(df_rej_reason)
   
