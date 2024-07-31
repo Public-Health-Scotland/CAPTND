3423fb07d7bb6bd5c41e1c879f4f4d1dddbaf5dc
charlie-smith <charlie.smith2@phs.scot>
2024-07-31 15:50:29 +0100

    Tidying pub tables

--- 07_publication/update_2024_06/functions/create_table_acc_rate.R
+++ 07_publication/update_2024_06/functions/create_table_acc_rate.R
@@ -23,23 +23,31 @@ create_table_acceptance_rate <- function(){
                                  levels = c('Referral accepted', "Referral not accepted", "Other"))) |> 
     group_by(dataset_type, hb_name, ref_acc_desc) |> 
     summarise(count = sum(count, na.rm = TRUE)) |> 
-    pivot_wider(names_from = ref_acc_desc, values_from = count,
-                values_fill = 0
-                ) |>
-    mutate(total = `Referral accepted` + `Referral not accepted`+ Other,
-           prop_accepted = paste0(round(`Referral accepted` / total * 100, 1), "%")) |>
-    mutate(`Referral accepted` = format(`Referral accepted`, big.mark = ","),
-           `Referral not accepted` = format(`Referral not accepted`, big.mark = ","),
-           `Other` = format(`Other`, big.mark = ","),
-           `total` = format(`total`, big.mark = ",")) |> 
+    pivot_wider(names_from = ref_acc_desc, values_from = count, 
+                values_fill = 0) |>
     right_join(df_ds_hb_name, by = c("dataset_type", "hb_name")) |> 
     mutate(hb_name = factor(hb_name, levels = hb_vector)) |> 
     arrange(dataset_type, hb_name) |> 
+    mutate(hb_name = as.character(hb_name)) |> 
+    as.data.frame() |> 
+    mutate(total = `Referral accepted` + `Referral not accepted`+ Other,
+           prop_accepted = as.double(round(`Referral accepted` / total * 100, 1))) 
+    
+  df_acc[is.na(df_acc)] <- ".."
+  df_acc[df_acc == 0] <- "-" 
+    
+  df_acc <- df_acc |> 
+    mutate(prop_accepted = paste0(prop_accepted, "%"), 
+           `Referral accepted` = format(`Referral accepted`, big.mark = ","),
+           `Referral not accepted` = format(`Referral not accepted`, big.mark = ","),
+           `Other` = format(`Other`, big.mark = ","),
+           `total` = format(`total`, big.mark = ",")) |> 
     rename(`Health board` = hb_name, Accepted = `Referral accepted`, 
            `Not accepted` = `Referral not accepted`, Total = total, 
            `Acceptance rate` = prop_accepted)
   
-    df_acc[is.na(df_acc)] <- ".."
+  df_acc[df_acc == "..%"] <- ".." 
+  df_acc[df_acc == "-%"] <- "0.0%"   
   
     save_as_parquet(df_acc, paste0(non_acc_dir, "table_acc_rate"))
   
