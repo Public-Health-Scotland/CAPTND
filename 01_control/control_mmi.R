c50373f0f1369d649efaece0729311d27a93eb24
bex-0-madden <bex.madden@phs.scot>
2024-09-19 13:12:43 +0100

    initial work patients seen for mmi

--- 01_control/control_mmi.R
+++ 01_control/control_mmi.R
@@ -17,8 +17,10 @@ month_end <- "2024-08-01"
 
 # functions?
 source("./07_publication/script/functions/summarise_appointments_att.R")
+source("./07_publication/script/functions/summarise_patients_seen.R")
 source("./07_publication/script/functions/get_appointments_df.R")
 source("./07_publication/script/functions/add_sex_description.R")
+source("./07_publication/script/functions/create_plots_patients_seen.R")
 source("./02_setup/save_df_as_parquet.R")
 
 # constants?
@@ -28,8 +30,11 @@ date_range <- seq.Date(from = month_start, to = month_end, by = "month")
 
 # Step 3 - Analyse Data ---------------------------------------------------
 
-summarise_appointments_att() # key output will be "/appointments_att/apps_att_mth_hb.parquet"
+summarise_appointments_att() # key output "/appointments_att/apps_att_mth_hb.parquet"
+summarise_patients_seen() # key outputs "/patients_seen/pat_seen_unadj_wait_grp_mth.parquet' and "/patients_seen/pat_seen_adj_wait_grp_mth.parquet'
 
+# Step 4 - Create plots ---------------------------------------------------
 
-
+create_plots_patients_seen("PT")
+create_plots_patients_seen("CAMHS")
 
