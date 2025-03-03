85f911d1862b70a09422f60429673210538c573f
lukeata <luke.taylor@phs.scot>
2025-03-03 09:19:19 +0000

    update_dag_ucpn_function

--- 04_check_modify/d&g_ucpn_update_fix.R
+++ 04_check_modify/d&g_ucpn_update_fix.R
@@ -11,19 +11,28 @@
 # provide the old-style UCPN in the UPI field. 
 
 # Solution: for submission from 2024-10-24 onwards, ensure old UCPN (in UPI field)
-# for D&G pathways IF pwathway has info from prior 2024-10-24.
+# for D&G pathways IF pathway has info from prior 2024-10-24.
+
+#Update: D&G did not provide old UCPN in UPI field, so a lookup table has been provided
+#which should contain all patients affected by the migration to Morse.
 
 dumfries_ucpn_fix <- function(df){
   
+  dag_ucpn_fix <- read.xlsx("/PHI_conf/MentalHealth5/CAPTND/CAPTND_shorewise/data/dumfries_morse_migration_lookup.xlsx") |>
+    janitor::clean_names() |>
+    mutate(across(everything(), as.character))
+  
   df_fixed <- df |>
     
-    group_by(across(all_of(data_keys))) |> # group by pathway (since upis can be across multiple ucpns)
+    left_join(dag_ucpn_fix, by = c("patient_id" = "patient_chi", "ucpn" = "morse_ucpn")) |>
+    
+    group_by(across(all_of(data_keys))) |> # group by pathway
     
     mutate(!!ucpn_o := case_when( # overwrite ucpn...
       
       !!sym(hb_name_o) == "NHS Dumfries and Galloway" & !!sym(dataset_type_o) == "PT" & # for NHS D&G PT
-        !!sym(ucpn_o) != !!sym(upi_o) # when ucpn and upi do not match
-               ~ !!sym(upi_o), # use old ucpn (upi) in place of new ucpn 
+        !!sym(ucpn_o) != migrated_from_ucpn # when ucpn and upi do not match
+               ~ migrated_from_ucpn, # use old ucpn in place of new ucpn 
       
       TRUE ~ !!sym(ucpn_o))) |> # otherwise keep ucpn as it is
   
