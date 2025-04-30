c0e82746c1e505c543b4df3f318a04c7c4614ce7
Alan Coventry <alan.coventry@phs.scot>
2025-04-30 14:57:49 +0000

    Multiple options for cancellation_date function

--- 10_pre_shorewise_dq/assess_cancellation_date.R
+++ 10_pre_shorewise_dq/assess_cancellation_date.R
@@ -13,9 +13,38 @@ assess_cancellation_date <- function(df){
     mutate(check_cancellation_date = case_when(
       !!sym(att_status_o) %in% c("02", "03", "09") & !is.na(!!sym(cancellation_date_o)) ~ "valid", # cancelled by clinic, by patient, or patient died AND has cancel date
       !!sym(att_status_o) %in% c("02", "03", "09") & is.na(!!sym(cancellation_date_o)) ~ "missing",
-      !!sym(att_status_o) %in% c("01", "05", "08") & is.na(!!sym(cancellation_date_o)) ~ "missing but valid",
-      TRUE ~ "missing but valid"))
+      !!sym(att_status_o) %in% c("01", "05", "08") & is.na(!!sym(cancellation_date_o)) ~ "valid",
+      TRUE ~ "invalid"))
 
   return(df_cancellation_date)
-  
+
 }
+
+
+# assess_cancellation_date <- function(df){
+# 
+#   df_cancellation_date <- df |>
+#     mutate(check_cancellation_date = case_when(
+#       !!sym(att_status_o) %in% c("02", "03", "09") & !is.na(!!sym(cancellation_date_o)) ~ "valid", # cancelled by clinic, by patient, or patient died AND has cancel date
+#       !!sym(att_status_o) %in% c("02", "03", "09") & is.na(!!sym(cancellation_date_o)) ~ "missing",
+#       !!sym(att_status_o) %in% c("01", "05", "08") & is.na(!!sym(cancellation_date_o)) ~ "missing but valid",
+#       TRUE ~ "missing but valid"))
+# 
+#   return(df_cancellation_date)
+# 
+# }
+
+
+
+# assess_cancellation_date <- function(df){
+#   
+#   df_cancellation_date <- df |>
+#     mutate(check_cancellation_date = case_when(
+#       !!sym(att_status_o) %in% c("02", "03") & !is.na(!!sym(cancellation_date_o)) ~ "valid", # cancelled by clinic, by patient, or patient died AND has cancel date
+#       !!sym(att_status_o) %in% c("02", "03") & is.na(!!sym(cancellation_date_o)) ~ "missing",
+#       !!sym(att_status_o) %in% c("01", "05", "08", "09") & is.na(!!sym(cancellation_date_o)) ~ "missing but valid",
+#       TRUE ~ "missing but valid"))
+#   
+#   return(df_cancellation_date)
+#   
+# }
