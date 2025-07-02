86f1e3aca5f4bfe9d55db233ae3b6858c1ecebed
lukeata <luke.taylor@phs.scot>
2025-07-02 09:22:20 +0000

    update_latest_date_function

--- 02_setup/data_analysis_latest_date.R
+++ 02_setup/data_analysis_latest_date.R
@@ -8,5 +8,6 @@
 data_analysis_latest_date <- list.dirs('../../../output', recursive = FALSE, full.names = FALSE) %>%  # list folders in directory
     grep("analysis", ., value = TRUE) %>% # get only extract folders
     str_replace(., "analysis_", "") %>% # remove "extract_" 
+    as.Date(.) %>%
     max(ymd(.)) # get latest folder for filepath
   
