71b03477462a13aa8eac458884248f0626b756a7
lukeata <luke.taylor@phs.scot>
2025-08-20 14:41:53 +0000

    add_new_dq_checks

--- 01_control/control_outputs.R
+++ 01_control/control_outputs.R
@@ -35,6 +35,7 @@ source('06_calculations/calculate_patient_turnover.R')
 source('06_calculations/get_latest_month_end.R')
 source("./05_data_quality/create_product_pack.R")
 source("./05_data_quality/create_product_pack_mth.R")
+source("09_ideas_space/check_impossible_dates.R")
 
 # 2 - open most recent RTT eval file--------------------------------------
 most_recent_month_in_data <- as.Date("2025-06-30") # maybe could derive value from the dated output folder name instead of human input here (could easily forget to update it)
@@ -47,24 +48,24 @@ df <- read_parquet(paste0(root_dir,'/swift_glob_completed_rtt.parquet')) |>
 
 #most_recent_month_in_data <- get_lastest_month_end(df)
 
-create_measure_reps = readline(prompt = 'Would you like to create the measure reports? - Y/N: ')
-
-if(create_measure_reps %in% c("Y", "y", "Yes", "yes")) {
-
-calculate_referrals(df, most_recent_month_in_data)
-calculate_open_cases(df, most_recent_month_in_data)
-calculate_patients_waiting(df, most_recent_month_in_data)
-calculate_patients_seen(df)
-calculate_appointments(df)
-calculate_attendance_status_rates(df)
-
-calculate_first_contact(df)
-calculate_first_treatment(df)
-calculate_pats_waiting_monthly(df)
-
-calculate_patient_turnover(df)
-
-}
+# create_measure_reps = readline(prompt = 'Would you like to create the measure reports? - Y/N: ')
+# 
+# if(create_measure_reps %in% c("Y", "y", "Yes", "yes")) {
+# 
+# calculate_referrals(df, most_recent_month_in_data)
+# calculate_open_cases(df, most_recent_month_in_data)
+# calculate_patients_waiting(df, most_recent_month_in_data)
+# calculate_patients_seen(df)
+# calculate_appointments(df)
+# calculate_attendance_status_rates(df)
+# 
+# calculate_first_contact(df)
+# calculate_first_treatment(df)
+# calculate_pats_waiting_monthly(df)
+# 
+# calculate_patient_turnover(df)
+# 
+# }
 
 # 2.2 Produce reports -----------------------------------------------------
 
@@ -82,19 +83,30 @@ create_product_pack_mth() # with product 2 as a monthly not quarterly heatmap
 
 # 2.3 Comparisons ---------------------------------------------------------
 
-create_comp_reps = readline(prompt = 'Would you like to create the comparison reports? - Y/N: ')
+# create_comp_reps = readline(prompt = 'Would you like to create the comparison reports? - Y/N: ')
+# 
+# if(create_comp_reps %in% c("Y", "y", "Yes", "yes")) {
+# 
+# compare_ref_aggregate_captnd()
+# compare_first_contact_aggregate_captnd()
+# compare_pat_seen_aggregate_captnd()
+# compare_dna_aggregate_captnd() # updated from Joana's original
+# compare_open_cases_aggregate_captnd()
+# compare_dna_new_return_app() # did not run - new_or_return_app_o not in attendance_status_rates.csv, att_cat instead?
+# compare_patients_waiting_monthly() # unadjusted
+# 
+# create_comparison_reports()
+# create_comparison_reports_patient_data()
+#
+#}
 
-if(create_comp_reps %in% c("Y", "y", "Yes", "yes")) {
+# 2.4 Data quality date checks
+month_start <- as.Date('2025-06-01')
+month_end <- as.Date('2025-06-30')
 
-compare_ref_aggregate_captnd()
-compare_first_contact_aggregate_captnd()
-compare_pat_seen_aggregate_captnd()
-compare_dna_aggregate_captnd() # updated from Joana's original
-compare_open_cases_aggregate_captnd()
-compare_dna_new_return_app() # did not run - new_or_return_app_o not in attendance_status_rates.csv, att_cat instead?
-compare_patients_waiting_monthly() # unadjusted
+impossible_app_dates()
+impossible_case_closed_dates()
+invalid_case_closed_dates()
+multiple_case_closed_dates()
+identify_duplicate_ucpns()
 
-create_comparison_reports()
-create_comparison_reports_patient_data()
-
-}
