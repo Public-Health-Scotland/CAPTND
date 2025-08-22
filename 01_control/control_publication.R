20171d2ab266e5bd0fc2efa17351ef587327e007
lukeata <luke.taylor@phs.scot>
2025-08-22 09:03:01 +0000

    update_pub_markdowns

--- 01_control/control_publication.R
+++ 01_control/control_publication.R
@@ -98,11 +98,11 @@ create_bar_chart_dna_simd("PT")
 create_trend_plot_dna_rate("CAMHS")
 create_trend_plot_dna_rate("PT")
 
-create_dot_plot_ethnicity("CAMHS")
-create_dot_plot_ethnicity("PT")
+# create_dot_plot_ethnicity("CAMHS")
+# create_dot_plot_ethnicity("PT")
 
-create_bar_chart_ethnicity("CAMHS")
-create_bar_chart_ethnicity("PT")
+# create_bar_chart_ethnicity("CAMHS")
+# create_bar_chart_ethnicity("PT")
 
 create_bar_chart_prot("CAMHS")
 #create_bar_chart_prot("PT")
