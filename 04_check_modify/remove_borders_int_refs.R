f2ea6e1b30f9e942c794ddd5ba5435adc163f21e
lukeata <luke.taylor@phs.scot>
2026-08-05 10:44:16 +0000

    update_borders_ref_removal_script

--- 04_check_modify/remove_borders_int_refs.R
+++ 04_check_modify/remove_borders_int_refs.R
@@ -9,16 +9,38 @@
 #These patients are therefore double counted
 #This function flags these patients, which can be identified as they have the referral source '06' CAMHS
 
+# remove_borders_int_refs <- function(df){
+# 
+# df_borders_refs <- df |>
+#   mutate(flag = case_when(!!sym(hb_name_o) == 'NHS Borders' & 
+#                             !!sym(ref_source_o) == 6 &
+#                             record_type_label == 'Referral' &
+#                             !!sym(dataset_type_o) == 'CAMHS' ~ 1,
+#                           TRUE ~ 0)) |>
+#   filter(flag != 1) |>
+#   select(-flag)
+# 
+#   return(df_borders_refs)
+# 
+# }
+
 remove_borders_int_refs <- function(df){
-
-df_borders_refs <- df |>
-  mutate(flag = case_when(!!sym(hb_name_o) == 'NHS Borders' & 
-                            !!sym(ref_source_o) == 6 &
-                            !!sym(dataset_type_o) == 'CAMHS' ~ 1,
-                          TRUE ~ 0)) |>
-  filter(flag != 1) |>
-  select(-flag)
-
+  
+  ref_condition <- if ("record_type_label" %in% names(df)) {
+    df$record_type_label == "Referral"
+  } else {
+    !is.na(df[[ref_rec_date_o]])
+  }
+  
+  df_borders_refs <- df |>
+    mutate(flag = case_when(!!sym(hb_name_o) == "NHS Borders" &
+                              !!sym(ref_source_o) == 6 &
+                              ref_condition &
+                              !!sym(dataset_type_o) == "CAMHS" ~ 1,
+                            TRUE ~ 0)) |>
+    filter(flag != 1) |>
+    select(-flag)
+  
   return(df_borders_refs)
-
+  
 }
